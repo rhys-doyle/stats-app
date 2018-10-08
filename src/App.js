c572fbd964bc2793e937e824c9e68dac917a91e8
Rhys Doyle <rhys.doyle@gmail.com>
2018-10-08 14:58:28 +1100

    routing and authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import Login from "./Components/Login/login";
+import Home from "./Components/Home/home";
+import Authentication from "./Components/Authentication/authentication";
 import styles from "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <Login />
-      </div>
+      <BrowserRouter>
+        <div className={styles.App}>
+          <Route exact path="/login" component={Login} />
+          <Authentication>
+            <Route exact path="/" component={Home} />
+          </Authentication>
+        </div>
+      </BrowserRouter>
     );
   }
 }
